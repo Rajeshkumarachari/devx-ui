8f4491ca32ff54e6652ac613a57dc813a4553207
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-10-05 20:49:22 +0530

    redux store created and user added to user

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,54 @@
+import { useSelector } from "react-redux";
+
+export default function NavBar() {
+  const user = useSelector((store) => store.user);
+  console.log("user" + user);
+  return (
+    <div className="navbar bg-gray-100">
+      <div className="flex-1">
+        <a className="  text-sky-800 mx-20 hover:bg-gray-300 px-2 py-1 rounded-md font-semibold cursor-pointer text-2xl">
+          DevX
+        </a>
+      </div>
+      {user && (
+        <div className="flex-none gap-2">
+          <div className="form-control"></div>
+
+          <div className="dropdown dropdown-end mx-10 flex justify-center">
+            <p className=" text-lg  mx-4 items-center flex">
+              Welcome,
+              <span className=" font-medium cursor-pointer hover:bg-gray-300 px-3 py-2  rounded-md">
+                {user?.firstName}
+              </span>
+            </p>
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src={user?.photoUrl} />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between text-lg">Profile</a>
+              </li>
+              <li>
+                <a className="text-lg">Settings</a>
+              </li>
+              <li>
+                <a className="text-lg hover:bg-red-400 hover:text-white">
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
